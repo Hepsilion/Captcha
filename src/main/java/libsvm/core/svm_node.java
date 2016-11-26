d66700dcb1bfb036e3dbc6455901d9c20ec0093b
outDream <2428091608@qq.com>
2016-11-26 12:02:45 +0800

    add support to libsvm3.2.1

---
+++ src/main/java/libsvm/core/svm_node.java
@@ -0,0 +1,6 @@
+package libsvm.core;
+public class svm_node implements java.io.Serializable
+{
+	public int index;
+	public double value;
+}
---
+++ src/main/java/libsvm/core/svm_print_interface.java
@@ -0,0 +1,5 @@
+package libsvm.core;
+public interface svm_print_interface
+{
+	public void print(String s);
+}
